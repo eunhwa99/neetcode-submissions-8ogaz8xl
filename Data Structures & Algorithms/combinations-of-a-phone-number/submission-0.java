975c1f7168c8d77fc3a02a6c206a4a9e381de599
eunhwa99 <68810660+eunhwa99@users.noreply.github.com>
2026-04-01 08:20:00 +0900

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+        List<String> res = new ArrayList<>();
+            private String[] digitToChar = {
+                        "", "", "abc", "def", "ghi", "jkl", "mno", "qprs", "tuv", "wxyz"
+                            };
+
+                                public List<String> letterCombinations(String digits) {
+                                        if(digits.isBlank()) return res;
+                                                backtrack(digits.toCharArray(), 0, new StringBuilder());
+                                                        return res;
+                                                            }
+
+                                                                void backtrack(char[] digits, int index, StringBuilder tmp) {
+                                                                        if (index >= digits.length) {
+                                                                                    res.add(tmp.toString());
+                                                                                                return;
+                                                                                                        }
+
+                                                                                                                int cur = digits[index] - '0';
+                                                                                                                        char[] letters = digitToChar[cur].toCharArray();
+                                                                                                                                for (int i = 0; i < letters.length; i++) {
+                                                                                                                                            tmp.append(letters[i]);
+                                                                                                                                                        backtrack(digits, index + 1, tmp);
+                                                                                                                                                                    tmp.deleteCharAt(tmp.length() - 1);
+                                                                                                                                                                            }
+                                                                                                                                                                                }
+                                                                                                                                                                                }
+
