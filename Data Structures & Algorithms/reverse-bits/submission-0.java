9768a24c5060bfccd1b8b8f929aaf2f11cd19e1d
eunhwa99 <68810660+eunhwa99@users.noreply.github.com>
2026-04-20 16:42:23 +0900

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,13 @@
+ class Solution {
+         public int reverseBits(int n) {
+                  int reverse = 0;
+                           for (int i = 0; i < 32; i++) {
+                                        reverse <<= 1;
+                                                     reverse = (reverse | (n & 1));
+                                                                  n >>= 1;
+                                                                           }
+                                                                                    return reverse;
+                                                                                         }
+                                                                                          }
+
+ 
